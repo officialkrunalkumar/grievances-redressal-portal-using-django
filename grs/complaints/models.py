945b22a140fb742fd58ec2c5cf3fa2cb71821273
Krunalkumar <krunalkumar@mountblue.io>
2021-06-14 12:17:55 +0530

    Basic Complaint Model Created

--- grs/complaints/models.py
+++ grs/complaints/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
+class complaints(models.Model):
+    """
+    This is the basic database for storing complaints.
+    """
+    title = models.CharField(max_length=200)
+    complaint_type = (
+        ('M', 'Management'),
+        ('E', 'Education'),
+        ('D', 'Deployment'),
+        ('S', 'Salary'),
+        ('G', 'General'),
+    )
+    complaint = models.CharField(
+        max_length=1,
+        choice=complaint_type,
+        default='G',
+    )
+    cohort_choice = (
+        ('P', 'Python'),
+        ('J', 'Java'),
+        ('R', 'React'),
+        ('A', 'Android'),
+        ('L', 'Linux'),
+        ('C', 'Cybersecurity'),
+        ('U', 'Unknown'),
+    )
+    cohort = models.CharField(
+        max_length=1,
+        choices=cohort_choice,
+        default='U',
+    )
+    complaint_level = (
+        ('I', 'Individual'),
+        ('G', 'General'),
+    )
+    level = models.CharField(
+        max_length=1,
+        choices=complaint_level,
+        default='I',
+    )
+    description = models.TextField(blank=False, max_length=500)
+    status = models.IntegerField(default=0)
