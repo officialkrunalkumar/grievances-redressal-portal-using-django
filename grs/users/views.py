b6f1a28b1bfb41d56f422da2e64a0800c623a651
Krunalkumar <krunalkumar@mountblue.io>
2021-06-15 18:31:15 +0530

    registration from view is generated

--- grs/users/views.py
+++ grs/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from .forms import UserRegisterForm
 # Create your views here.
+
+
+def register(request):
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, f"Congratulation! You are registed with MountBlue's GRS.")
+    return render(request, 'users/register.html', {'form': form})
