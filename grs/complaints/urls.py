bc6345e90f2ef02075094f71a3b23cc1e44186b2
Krunalkumar <krunalkumar@mountblue.io>
2021-06-16 18:30:04 +0530

    Update and Delete functionality added

--- grs/complaints/urls.py
+++ grs/complaints/urls.py
@@ -6,5 +6,7 @@ app_name = 'complaints'
 
 
 urlpatterns = [
-    path('add_complaint', views.add_complaint, name="add_complaint")
+    path('add_complaint', views.add_complaint, name="add_complaint"),
+    path('delete_complaint/<int:id>', views.delete_complaint, name="delete_complaint"),
+    path('update_complaint/<int:id>', views.update_complaint, name="update_complaint"),
 ]
--- grs/complaints/views.py
+++ grs/complaints/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied 
 # Create your views here.
 from .models import Complaint
 from .forms import AddComplaintForm
@@ -26,3 +27,42 @@ def add_complaint(request):
             complaint.save()
             messages.success(request, 'Complaint Registered')
     return render(request, 'complaints/add_complaint.html', { 'form':form }) 
+
+
+@login_required(login_url='login')
+def delete_complaint(request, id):
+    complaint = get_object_or_404(Complaint, id=id)
+    if request.user == complaint.user:
+        complaint.delete()
+        messages.success(request, "Deleted Succesfully!")
+        return redirect('users:profile')
+    else:
+        raise PermissionDenied("You Don't Have Persmission to do that") 
+
+
+@login_required(login_url='login')
+def update_complaint(request, id):
+    complaint = get_object_or_404(Complaint, id=id)
+    if request.user == complaint.user:
+        if request.method == 'POST':
+            form = AddComplaintForm(request.POST, instance=request.user)
+            if form.is_valid():
+                complaint = Complaint(
+                                id = complaint.id,
+                                title=request.POST['title'],
+                                name=request.POST['name'],
+                                complaint_for=request.POST['complaint_for'],
+                                complaint_to=request.POST['complaint_to'],
+                                cohort=request.POST['cohort'],
+                                level=request.POST['level'],
+                                description=request.POST['description'],
+                                category=request.POST['category'],
+                                user=request.user
+                            )
+                complaint.save()
+                messages.success(request, 'Complaint Registered')
+        else:
+            form = AddComplaintForm(instance = complaint)
+        return render(request, 'complaints/add_complaint.html', {'form': form})
+    else:
+        raise PermissionDenied("You Don't Have Persmission to do that") 
