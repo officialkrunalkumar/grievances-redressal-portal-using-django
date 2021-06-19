fcd48c8a8cfea71f0e34133aab1c8b33eb70fdfa
Krunalkumar <krunalkumar@mountblue.io>
2021-06-19 17:33:01 +0530

    feedback fegistered in admin

--- grs/complaints/admin.py
+++ grs/complaints/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Complaint
+from .models import Complaint, Feedback
 
 
 class ComplaitAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'complaint_to', 'complaint_for', 'user_role')
 
-admin.site.register(Complaint, ComplaitAdmin) 
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'rating', 'description')
+
+
+admin.site.register(Complaint, ComplaitAdmin)
+admin.site.register(Feedback, FeedbackAdmin)  
