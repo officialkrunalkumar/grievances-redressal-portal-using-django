7599c132de4dd36c1a31e17dc57fa909d3c7f639
Krunalkumar <krunalkumar@mountblue.io>
2021-06-19 17:34:08 +0530

    feedback form generated

--- grs/complaints/forms.py
+++ grs/complaints/forms.py
@@ -1,5 +1,5 @@
 from django.forms import fields
-from .models import Complaint
+from .models import Complaint, Feedback
 from django import forms
 
 
@@ -8,3 +8,10 @@ class AddComplaintForm(forms.ModelForm):
     class Meta:
         model = Complaint
         fields = ['title', 'name', 'complaint_for', 'complaint_to', 'cohort', 'level', 'description', 'category']
+
+
+class AddFeedbackForm(forms.ModelForm):
+
+    class Meta:
+        model = Feedback
+        fields = ['rating','description']
