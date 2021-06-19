93a6f702abbb89577f39400ed4fba23010356eef
Krunalkumar <krunalkumar@mountblue.io>
2021-06-19 17:54:10 +0530

    profile added, forms added

--- grs/users/views.py
+++ grs/users/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UpdateUserForm, UpdateProfileForm
 # Create your views here.
 from complaints.models import Complaint
 
@@ -17,9 +17,27 @@ def register(request):
 
 @login_required(login_url='login')
 def profile(request):
+    ''' Updating profile page and
+    redinring the profile page'''
+    if request.method == 'POST':
+        p_form = UpdateProfileForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile)
+        u_form = UpdateUserForm(request.POST, instance=request.user)
+        if u_form.is_valid() and p_form.is_valid():
+            p_form.save()
+            u_form.save()
+            messages.success(request, 'Your Account has been Update')
+            return redirect('users:profile')
+    else:
+        p_form = UpdateProfileForm(instance=request.user.profile)
+        u_form = UpdateUserForm(instance=request.user)
     complaints = Complaint.objects.all().filter(user=request.user).order_by('-id')
     print(complaints)
     context = {
-        'complaints': complaints
+        'p_form': p_form,
+        'u_form': u_form,
+        'complaints': complaints,
     }
     return render(request, 'users/profile.html', context)
