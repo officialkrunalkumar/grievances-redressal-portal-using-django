a9ef26fe7082d39024e312089e5e881350f72e1f
Krunalkumar <krunalkumar@mountblue.io>
2021-06-22 16:05:22 +0530

    flake8 complaint

--- grs/grs/urls.py
+++ grs/grs/urls.py
@@ -22,8 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
-    path('login/', auth_views.
-         LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='users/login.html'),
+         name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('', include('home.urls')),
     path('complaints/', include('complaints.urls')),
