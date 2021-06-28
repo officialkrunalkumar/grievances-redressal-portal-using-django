e64797f64c717eb11a10def5b1d97f8f85ecd05a
Krunalkumar <krunalkumar@mountblue.io>
2021-06-28 11:19:39 +0530

    user name added in admin to make it easy for read.
    feature suggested by Parul

--- grs/complaints/admin.py
+++ grs/complaints/admin.py
@@ -6,7 +6,7 @@ from .models import Complaint, Feedback
 
 class ComplaitAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'complaint_to',
-                    'complaint_for', 'user_role')
+                    'complaint_for', 'user_role', 'user')
 
 
 class FeedbackAdmin(admin.ModelAdmin):
